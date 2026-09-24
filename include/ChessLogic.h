1a993f1970e57bcb70f7637bdba645b18e3d122b
Zekaroni <ezekiel.hall.04@gmail.com>
2026-09-23 20:15:38 -0400

    added piece bitboards

--- include/ChessLogic.h
+++ include/ChessLogic.h
@@ -1,7 +1,24 @@
+#include <cstdint>
+
+#define WHITE 0
+#define BLACK 1
+
+struct pieceBitboard
+{
+    uint64_t pawnBitborad;
+    uint64_t knightBitborad;
+    uint64_t bishopBitborad;
+    uint64_t rookBitborad;
+    uint64_t queenBitborad;
+    uint64_t kingBitborad;
+};
+
+
 class ChessLogic
 {
 private:
-    int x;
+    pieceBitboard pieceBitboards[2];
+
 public:
     void printHello();
 };
