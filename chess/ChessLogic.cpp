109bd26ced317b08d503016737e2a768d9571c59
Zekaroni <ezekiel.hall.04@gmail.com>
2026-09-26 04:27:20 -0400

    automated push

--- chess/ChessLogic.cpp
+++ chess/ChessLogic.cpp
@@ -45,10 +45,10 @@ bitboard_t ChessLogic::getPiecePositionBitboard(piece_t piece, int position)
         int y = int(i/8); // Gets the column
         int x = int(i%8); // Gets the row
        // std::array<Point,8> point = {{{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,1},{-1,-1},{1,-1}}}; // the knights offsets
-        for (int j=0;j<64;j++) // loops though each offset
+        for (int j=64;j>=0;j--) // loops though each offset
         {
-            int currentX = int(j%8);
             int currentY = int(j/8);
+            int currentX = int(j%8);
             if (((currentX == x) && (currentY != currentY)) || ((currentY == y) && (currentX != x))) // checks if the offset plus the current square is a valid square
             {
                 currentBoard |= (uint64_t)1<<j; // appends the legal kngiht offsets to the bitboard
