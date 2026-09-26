6452d08d4e023b0587559f1d838d7378c8647914
Zekaroni <ezekiel.hall.04@gmail.com>
2026-09-26 05:13:44 -0400

    automated push

--- chess/ChessLogic.cpp
+++ chess/ChessLogic.cpp
@@ -4,7 +4,8 @@ ChessLogic::ChessLogic()
 {   
     pieceBitmapLookup[CHESS_GLOBALS::PIECES::KNIGHT] = knightPositions;
     pieceBitmapLookup[CHESS_GLOBALS::PIECES::KING] = kingPositions;
-    boardState[0] = CHESS_GLOBALS::PLAYER_PIECES::WHITE_KING;
+    pieceBitmapLookup[CHESS_GLOBALS::PIECES::ROOK] = rookPositions;
+    boardState[0] = CHESS_GLOBALS::PLAYER_PIECES::WHITE_ROOK;
     calculateMoves();
 
 }
@@ -39,23 +40,24 @@ bitboard_t ChessLogic::getPiecePositionBitboard(piece_t piece, int position)
 // Beautiful function to derive moves
  void ChessLogic::calculateMoves()
  {
-    for (int i = 64; i >= 0; i--) // Loops through all squares
+    for (int i = 63; i >= 0; i--) // Loops through all squares
     {
         bitboard_t currentBoard = {0};
         int y = int(i/8); // Gets the column
         int x = int(i%8); // Gets the row
        // std::array<Point,8> point = {{{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,1},{-1,-1},{1,-1}}}; // the knights offsets
-        for (int j=64;j>=0;j--) // loops though each offset
+        for (int j=0;j<64;j++) // loops though each offset
         {
             int currentY = int(j/8);
             int currentX = int(j%8);
-            if (((currentX == x) && (currentY != currentY)) || ((currentY == y))) // checks if the offset plus the current square is a valid square
+            if (currentY == y && currentX != x || currentX == x && currentY != y) // checks if the offset plus the current square is a valid square
             {
                 currentBoard |= (uint64_t)1<<j; // appends the legal kngiht offsets to the bitboard
             }
         }
+        std::cout << currentBoard << "ULL,\n" << std::endl;
         print_bitboard(currentBoard);
-        std:: cout << "\n" << std::endl;
-        //rookPositions[i] =  currentBoard;
+        // std:: cout << "\n" << std::endl;
+        rookPositions[i] =  currentBoard;
     }
  }
